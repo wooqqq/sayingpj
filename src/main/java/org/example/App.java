0b157054f704ede03a404f909a19984231d22106
깃wooqqq <sge0225@naver.com>
2023-03-02 10:32:41 +0900

    work5_1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -10,9 +12,10 @@ public class App {
     }
 
     public void run() {
-        System.out.println("== 명언 ==");
+        System.out.println("== 명언 앱 ==");
 
-        long lastWiseSayingId = 1;
+        long lastWiseSayingId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
@@ -21,14 +24,22 @@ public class App {
             if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
+                long id = lastWiseSayingId + 1;
                 System.out.printf("명언 : ");
                 String content = sc.nextLine().trim();
                 System.out.printf("작가 : ");
                 String authorName = sc.nextLine().trim();
 
+                WiseSaying wiseSaying = new WiseSaying(id, content, authorName);
+                wiseSayings.add(wiseSaying);
 
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastWiseSayingId);
-                lastWiseSayingId++;
+                // 리스트로 만들어야한다.
+                // new ArrayList<>();
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                lastWiseSayingId = id; // 증가
+            } else if (command.equals("목록")) {
+                System.out.printf("생성된 명언 수 : %d\n", wiseSayings.size());
             }
         }
 
