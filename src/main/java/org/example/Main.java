c34233e443d088f1b59b2bfdaef95d860a3c39c4
깃wooqqq <sge0225@naver.com>
2023-03-02 09:32:01 +0900

    work3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String order;
+        String saying;
+        String writer;
+        String order2;
+
+        System.out.println("== 명언 앱 ==");
+        System.out.printf("명령) ");
+        order = sc.nextLine();
+
+        System.out.printf("명언 : ");
+        saying = sc.nextLine();
+        System.out.printf("작가 : ");
+        writer = sc.nextLine();
+        System.out.println("1번 명언이 등록되었습니다.");
+
+        System.out.printf("명령) ");
+        order2 = sc.nextLine();
     }
 }
