e99ce309fc985ac1a49439f752604d623f912e4b
깃wooqqq <sge0225@naver.com>
2023-03-02 10:21:16 +0900

    work3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,18 +12,23 @@ public class App {
     public void run() {
         System.out.println("== 명언 ==");
 
+        long lastWiseSayingId = 1;
+
         while (true) {
-            System.out.printf("명령) ");
+            System.out.print("명령) ");
             String command = sc.nextLine().trim();
 
             if (command.equals("종료")) {
                 break;
-            }
-            else if (command.equals("등록")) {
+            } else if (command.equals("등록")) {
                 System.out.printf("명언 : ");
                 String content = sc.nextLine().trim();
                 System.out.printf("작가 : ");
                 String authorName = sc.nextLine().trim();
+
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastWiseSayingId);
+                lastWiseSayingId++;
             }
         }
 
